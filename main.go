40c25b4ac0a9137b5abce935c03b93e55d6abb2d
bji <robinj0726@hotmail.com>
2022-07-31 11:05:05 +0800

    read from config file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -8,11 +10,29 @@ import (
 	"github.com/robinj730/rtc-client-go/client"
 )
 
+type Config struct {
+	RemoteAddr string `json:"server"`
+	RemotePort int    `json:"port"`
+	Secure     bool   `json:"secure"`
+}
+
 func main() {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	c := Config{}
+
+	err = json.Unmarshal(file, &c)
+	if err != nil {
+		panic(err)
+	}
+
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	client, err := client.NewClient("localhost", 4000, true)
+	client, err := client.NewClient(c.RemoteAddr, c.RemotePort, c.Secure)
 	if err != nil {
 		panic(err)
 	}
